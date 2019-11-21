c7eb4160a08342d23311ab51ad4f275c5fce6abd
Alex <zaychenko96y@outlook.com>
2019-11-21 19:54:58 +0200

    Refactored callback in promisifier

--- promisifier.js
+++ promisifier.js
@@ -1,6 +1,4 @@
-const logMsg = (msg, cb) => {
-  setTimeout(() => cb(null, msg), 100)
-}
+const logMsg = (msg, cb) => setTimeout(() => cb(null, msg), 100)
 
 const promisify = (cb, ...args) => new Promise((resolve, reject) =>
   cb.apply(this, [...args, (err, result) => err ? reject(err) : resolve(result)]))
