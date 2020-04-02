915942712bbe5c6aa97f1a9a7ca1c6a89037f5dc
Alex <zaychenko96y@outlook.com>
2020-04-02 23:51:12 +0300

    Refactored a bit

--- sortListFromMicrosoftWord.js
+++ sortListFromMicrosoftWord.js
@@ -6,8 +6,7 @@ const sortList = input => {
   const [_, ...data] = input.split(/\n\d+\.\s+(?=[a-zA-ZА-Яа-я])/g)
   const cyrillicSet = data.filter(item => item.charCodeAt(0) > lastLatinLetterCode).sort()
   const latinSet = data.filter(item => item.charCodeAt(0) <= lastLatinLetterCode).sort()
-  const result = [...cyrillicSet, ...latinSet]
-    .join('\n')
+  const result = [...cyrillicSet, ...latinSet].join('\n')
   console.log('Writing data to the file...')
   fs.writeFile('result.txt', result, () => console.log('Done.'))
 }
